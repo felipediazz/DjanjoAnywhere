8c6d65d941e429c948003779d5566f789fa2c413
felipediazz <pipediaz1415@gmail.com>
2023-04-28 15:35:42 -0400

    view home

--- core/views.py
+++ core/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+def home(request):
 
-def obtener_imagen(request):
-    with open('/ruta/a/la/imagen.jpg', 'rb') as f:
-        imagen_data = f.read()
-    return HttpResponse(imagen_data, content_type="image/jpeg")
+    return render(request, 'core/home.html')
