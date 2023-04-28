56215c0ccdb64d50570adc24871c67b07d7895ef
felipediazz <pipediaz1415@gmail.com>
2023-04-28 16:05:38 -0400

    home

--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
 
-def home(request):
+##def home(request):
 
+##    return render(request, 'core/home.html')
+
+def home(request):
+    if request.method == 'POST' and request.FILES['imagen']:
+        imagen = request.FILES['imagen'].read()
+        url = 'http://localhost:8888/upload'
+        files = {'imagen': imagen}
+        response = requests.post(url, files=files)
+        if response.status_code == 200:
+            imagen_variable = response.json()['imagen']
+            # Haz algo con la variable de imagen aquí
     return render(request, 'core/home.html')
