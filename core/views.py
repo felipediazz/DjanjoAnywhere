3ea61693e5ef3063ea0a68c33fddd9d91831c339
felipediazz <pipediaz1415@gmail.com>
2023-05-02 17:42:09 -0400

    metodos incompletos

--- core/views.py
+++ core/views.py
@@ -12,6 +12,7 @@ import os
 
 from nbconvert import PythonExporter
 import nbformat
+
 # Create your views here.
 
 ##def home(request):
@@ -25,17 +26,17 @@ def index(request):
 
 def process_image(request):
     if request.method == 'POST' and request.FILES['image']:
-        # Guarda la imagen cargada en el sistema de archivos
+        # SE GUARDA LA IMAGEN
         myfile = request.FILES['image']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
 
-        # Lee la imagen guardada y la convierte a un arreglo de Numpy
+        # SE CARGA LA IMAGEN EN UN ARREGLO NUMPY
         image_path = os.path.join(settings.MEDIA_ROOT, filename)
         image = cv2.imread(image_path)
 
-        # Procesa la imagen en Jupyter Notebook y almacena el resultado
+        # SE PROCESA LA IMAGEN CON EL NOTEBOOK JUPYTER
         result_path = os.path.join(settings.MEDIA_ROOT, 'result.csv')
         with open('Sam.ipynb') as f:
             nb = nbformat.read(f, as_version=4)
@@ -47,25 +48,21 @@ def process_image(request):
 
         result_df.to_csv(result_path, index=False)
 
-        # Elimina los archivos temporales creados
+        # SE ELIMINA LAS IMAGENES ALMACENADAS
         os.remove(image_path)
         os.remove(result_path)
 
-        # Renderiza el resultado en un template
+        # SE MUESTRA LA IMAGEN
         return render(request, 'core/result.html', {'result': result_df})
 
-    #   # Lee el resultado del procesamiento desde el archivo generado por Jupyter Notebook
     #     result_path = os.path.join(settings.MEDIA_ROOT, 'result.csv')
     #     result_df = pd.read_csv(result_path)
 
-    #     # Elimina los archivos temporales creados
     #     os.remove(image_path)
     #     os.remove(result_path)
 
-    #     # Renderiza el resultado en un template
     #     return render(request, 'result.html', {'result': result_df})
 
     else:
-        # Si el formulario no es enviado correctamente, vuelve a mostrar el formulario
         form = ImageUploadForm()
         return render(request, 'core/index.html', {'form': form})
